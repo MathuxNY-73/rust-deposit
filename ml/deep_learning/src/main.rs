0a2d085a0aae7eda0565c312efc109b37296c309
Antoine Pouillaude <a.pouillaude@nomagic.ai>
2021-10-05 22:00:57 +0200

    Start to extract functions from localize container

--- ml/deep_learning/src/main.rs
+++ ml/deep_learning/src/main.rs
@@ -1,5 +1,12 @@
 use ndarray::prelude::*;
 
+fn transform_point_cloud(point_cloud: Array2<f64>, translation: &Array2<f64>, rotation: &Array2<f64>) -> Array2<f64> {
+    let mut results = point_cloud.dot(&translation.t());
+    results += rotation;
+
+    results
+}
+
 fn main() {
     let test_array = array![[1.,2.,3.], [4.,5.,6.]];
     let other = array![[2.,3.,4.], [5.,6.,7.]];
